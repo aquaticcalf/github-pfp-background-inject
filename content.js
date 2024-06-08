cf9dc5a730635bc603eeadc9ec87c168a098a082
sam <aquaticcalf@proton.me>
2024-06-08 10:38:45 +0530

    meow

--- content.js
+++
@@ -1,9 +0,0 @@
-const body = document.getElementsByTagName('body')[0]
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        body.style.background = url(response.msg)
-        sendResponse({ msg : "img set" })
-    }
-)
-  
